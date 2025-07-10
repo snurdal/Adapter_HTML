045aec4069db38702ebdb4dbc099b3ccbb066831
Seyma Nur Dalamanli <seymanurdalamanli@gmail.com>
2025-07-10 23:58:01 +0300

    Add project files.

---
+++ HtmlAdapterSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace Adapter_HTML
+{
+    public class HtmlAdapterSettings
+    {
+        public required string BaseURL { get; set; }
+        public string? Endpoint { get; set; }
+        public string? XPath { get; set; }
+    }
+}
---
+++ HtmlToJsonConverter.cs
@@ -0,0 +1,47 @@
+﻿using HtmlAgilityPack;
+using Microsoft.Extensions.Options;
+
+namespace Adapter_HTML
+{
+    public class HtmlToJsonConverter
+    {
+        private readonly HtmlAdapterSettings settings;
+        private readonly HttpClient client;
+
+        public HtmlToJsonConverter(IOptions<HtmlAdapterSettings> options, IHttpClientFactory factory)
+        {
+            settings = options.Value;
+            client = factory.CreateClient();
+        }
+
+        public async Task<IEnumerable<TagModel>> ConvertAsync()
+        {
+            string url = $"{settings.BaseURL} {settings.Endpoint ?? ""}";
+            string html = await client.GetStringAsync(url);
+
+            var doc = new HtmlDocument();
+            doc.LoadHtml(html);
+            IEnumerable<HtmlNode> tags;
+            if (!string.IsNullOrEmpty(settings.XPath))
+            {
+                tags = doc.DocumentNode.SelectNodes(settings.XPath) ?? Enumerable.Empty<HtmlNode>();
+            }
+            else 
+            {
+                tags = doc.DocumentNode.Descendants();
+            }
+
+            var jsonElements = tags.Where(x => !x.Name.StartsWith("#")).Select(tag => new TagModel
+                {
+                    TagName = tag.Name,
+                    InnerText = tag.InnerText.Trim(),
+                    Attributes = tag.Attributes.Select(attr => new AttributeModel
+                    {
+                        Name = attr.Name,
+                        Value = attr.Value
+                    })
+                 });
+            return jsonElements;
+        }
+    }
+}
